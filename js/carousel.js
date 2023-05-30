bab6398070996981be68eed6a9e58fb3c42c2bce
rakakroma <102795114+rakakroma@users.noreply.github.com>
2023-05-30 22:42:19 +0800

    update status, carousel change to scrollable container in non-mobile viewports

--- js/carousel.js
+++ js/carousel.js
@@ -1,21 +1,51 @@
 import { closeNavPopUp } from "./nav-hamburger.js";
 
+const debounce = (func, delay = 200) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
+
 // Get a reference to the swiper container element
 const swiperContainer = document.querySelector(".swiper-container");
 
 const getSlidesPerView = () => {
   const viewportWidth = window.innerWidth;
   switch (true) {
-    case viewportWidth >= 768:
-      return 3;
-    case viewportWidth >= 620:
+    // case viewportWidth >= 768:
+    //   return 3;
+    case viewportWidth >= 640:
       return 2;
     default:
       return 1;
   }
 };
 
-let swiper;
+const removeAllSwiperClass = () => {
+  document.querySelector(".swiper-container").className =
+    "former-swiper-container";
+  document.querySelector(".swiper-wrapper").className =
+    "swiper-wrapper-element horizontal-scroll-container";
+  document.querySelector(".swiper-wrapper-element").style = "";
+  document.querySelectorAll(".card").forEach((cardEle) => {
+    cardEle.className = "card";
+    cardEle.style = "";
+  });
+};
+
+const restoreAllSwiperClass = () => {
+  document.querySelector(".former-swiper-container").className =
+    "swiper-container";
+  document.querySelector(".swiper-wrapper-element").className =
+    "swiper-wrapper-element swiper-wrapper";
+  document.querySelectorAll(".card").forEach((cardEle) => {
+    cardEle.className = "card swiper-slide";
+  });
+};
 
 const createSwiper = () => {
   swiper = new Swiper(swiperContainer, {
@@ -28,26 +58,39 @@ const createSwiper = () => {
   });
 };
 
+const stopSwiper = () => {
+  removeAllSwiperClass();
+  swiper.disable();
+};
+
+const restartSwiper = () => {
+  restoreAllSwiperClass();
+  swiper.enable();
+};
+
+let swiper;
+
 createSwiper();
 
-function debounce(func, delay = 200) {
-  let timeoutId;
-  return function (...args) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      func.apply(this, args);
-    }, delay);
-  };
+if (window.innerWidth > 768) {
+  stopSwiper();
 }
 
-window.addEventListener(
-  "resize",
+$(window).resize(
   debounce(() => {
     if ($(".header--expanded").length) {
       closeNavPopUp();
     }
-
-    swiper.params.slidesPerView = getSlidesPerView();
-    swiper.update();
+    if (window.innerWidth >= 768) {
+      if (swiper.enabled) {
+        stopSwiper();
+      }
+    } else {
+      if (!swiper.enabled) {
+        restartSwiper();
+      }
+      swiper.params.slidesPerView = getSlidesPerView();
+      swiper.update();
+    }
   })
 );
